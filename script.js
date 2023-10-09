aad2791dcd1825f764f530c9fcd3f149cb1bf365
RanggaAgera <akuhpintar123@gmail.com>
2023-10-09 15:04:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+ alert("Hello, world!");
+ console.log("Hello, world!");
+
+ function test() {
+ let angka1= 10 
+let angka2 = 5
+let total = angka1 + angka2
+let nama = "Rangga Aditya"
+console.log(nama + "Total angka: " + total)}
+
+test()
