13b3b7461f20c365827f03d05270e14372fef73c
RanggaAgera <akuhpintar123@gmail.com>
2023-10-11 09:16:50 +0700

    First

--- script.js
+++ script.js
@@ -2,10 +2,61 @@
  console.log("Hello, world!");
 
  function test() {
- let angka1= 10 
+let angka1= 10 
 let angka2 = 5
 let total = angka1 + angka2
 let nama = "Rangga Aditya"
 console.log(nama + "Total angka: " + total)}
 
 test()
+
+let iLikeAnime = true
+
+console.log(iLikeAnime)
+let youlikemeat=true
+if(youlikemeat){
+    console.log("You like meat");
+}
+else{
+    console.log("You dont like meat");
+}
+
+let umur=18
+console.log(umur)
+if (umur<=17){
+    console.log("Anda belum memenuhi persyaratan untuk mendapatkan SIM");
+}
+else if (umur>=18){
+    console.log("Anda sudah bisa mendapat SIM");
+}
+
+let usia=1;
+while(usia<10){
+    console.log("Usia kamu sekarang" + usia + " Tahun");
+    usia++;
+}
+
+let nilai = 110;
+if(nilai>=70 || nilai<=100){
+    console.log("Selamat anda lulus");
+}
+
+else{
+    console.log("TIdak lulus");
+}
+
+for(i=0;i<10;i++){
+    console.log(i);
+}
+
+for(i=0;i<10;i++){
+    if(i%2){
+    console.log(i);}
+}
+
+for(i=0;i<10;i++){
+    if(i== 5){
+        console.log(i);
+        break;
+    }
+}
